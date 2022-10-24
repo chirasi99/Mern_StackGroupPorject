9a4d8bc50f617865fcb521b2be33b46da67a5555
Chirasi Amaya <87434721+chirasi99@users.noreply.github.com>
2022-10-25 00:07:27 +0530

    Update ViewTeacher.js file

--- client/src/componets/ViewTeacher.js
+++ client/src/componets/ViewTeacher.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
-export default class PostDetails extends Component {
+export default class ViewTeacher extends Component {
   constructor(props) {
     super(props);
 
